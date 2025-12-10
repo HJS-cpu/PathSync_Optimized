df27fe46828143d598408a0426da85e2c5869f7f
HJS-cpu <hajo.schlingensief@gmail.com>
2025-12-10 12:19:27 +0100

    Create wdlutf8.h

---
+++ WDL/wdlutf8.h
@@ -0,0 +1,10 @@
+#ifndef _WDLUTF8_H_
+#define _WDLUTF8_H_
+
+// Stub for wdlutf8.h - minimal definitions for win32_utf8.c
+
+#ifndef WDL_UTF8_MAXFNLEN
+#define WDL_UTF8_MAXFNLEN 2048
+#endif
+
+#endif
