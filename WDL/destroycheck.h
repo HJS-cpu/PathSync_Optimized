7b575fd3b6cc932b0bd65d0900ef2929b93f376a
HJS-cpu <hajo.schlingensief@gmail.com>
2025-12-10 12:16:51 +0100

    Create destroycheck.h

---
+++ WDL/destroycheck.h
@@ -0,0 +1,20 @@
+#ifndef _WDL_DESTROYCHECK_H_
+#define _WDL_DESTROYCHECK_H_
+
+// Stub for destroycheck.h - not needed for PathSync
+class WDL_DestroyCheck
+{
+public:
+  WDL_DestroyCheck() {}
+  ~WDL_DestroyCheck() {}
+  bool isOK() { return true; }
+};
+
+class WDL_DestroyState
+{
+public:
+  WDL_DestroyState() {}
+  bool isOK() { return true; }
+};
+
+#endif
